c9eb4ae898ccbc3d0638a2ee6d235f3c39dd3591
D. Dramchev <ddramchev@gmail.com>
2022-12-04 01:28:17 +0000

    Complete solution for day 3 part 1

--- 2022/typescript/src/day03/part1.test.ts
+++ 2022/typescript/src/day03/part1.test.ts
@@ -1,4 +1,6 @@
 import { findPrioritySum, producePriorityHashMap } from "./part1";
+import { readFileSync } from "fs";
+import path from "path";
 
 describe("producePriorityHash", () => {
   it("returns 42 for vJrwpWtwJgWrhcsFMMfFFhFp", () => {
@@ -25,5 +27,11 @@ wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\n\
 ttgJtRGJQctTZtZT\n\
 CrZsJsPPZsGzwwsLwLmpwMDw\n";
     expect(findPrioritySum(input)).toBe(157);
+  });
+
+  it("returns answer for puzzle", () => {
+    const filePath = path.join(__dirname, "../../../input/day03");
+    const fileString = readFileSync(filePath, "utf8");
+    expect(findPrioritySum(fileString)).toBe(7908);
   });
 });
