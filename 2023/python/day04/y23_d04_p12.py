973234c60a192eeccff07d04fb9108e23a9dd828
ddrmv <ddramchev@gmail.com>
2023-12-04 11:24:05 +0000

    refactor: Change cards from list to class, with type hints

--- 2023/python/day04/y23_d04_p12.py
+++ 2023/python/day04/y23_d04_p12.py
@@ -1,33 +1,46 @@
+from typing import List
+
+class Card():
+    def __init__(self, your_nums: List[int], win_nums: List[int], copies: int):
+        self.your_nums = your_nums
+        self.win_nums = win_nums
+        self.copies = copies
+
+    def get_hits_num(self):
+        return len(set(self.your_nums).intersection(set(self.win_nums)))
+        
+
 def process_input(input):
     cards = []
     for card in input.rstrip().split("\n"):
-        your_numbers_part, winning_numbers_part = card.split(': ')[1].split(' | ')
-        your_numbers = [int(num) for num in your_numbers_part.split(' ') if num.isdigit()]
-        winning_numbers = [int(num) for num in winning_numbers_part.split(' ') if num.isdigit()]
-        card_copies = 1
-        cards.append([your_numbers, winning_numbers, card_copies])
+        your_nums_part, win_nums_part = card.split(': ')[1].split(' | ')
+        your_nums = [int(num) for num in your_nums_part.split(' ') if num.isdigit()]
+        win_nums = [int(num) for num in win_nums_part.split(' ') if num.isdigit()]
+        copies = 1
+        cards.append(Card(your_nums, win_nums, copies))
     return cards
 
-def find_number_of_hits(card):
-    return len(set(card[0]).intersection(set(card[1])))
+def find_number_of_hits(card: Card):
+    return len(set(card.your_nums).intersection(set(card.win_nums)))
 
 def hits_to_value(hits):
     return hits if hits < 2 else 2**(hits - 1)
 
-def update_card_numbers(cards):
+def update_card_numbers(cards: List[Card]):
     for index, card in enumerate(cards):
-        hits = find_number_of_hits(card)
+        hits = card.get_hits_num()
         if hits != 0:
             for next_card_num in range(index + 1, index + 1 + hits):
                 if next_card_num < len(cards):
-                    cards[next_card_num][2] += card[2]
+                    cards[next_card_num].copies += card.copies
     
-def get_total_number_of_cards(cards):
-    return sum([card[2] for card in cards])
+def get_total_number_of_cards(cards: List[Card]):
+    return sum([card.copies for card in cards])
+
 
 def part1(input):
-    cards = process_input(input)
-    return sum(hits_to_value(find_number_of_hits(card)) for card in cards)
+    cards: List[Card] = process_input(input)
+    return sum(hits_to_value(card.get_hits_num()) for card in cards)
 
 def part2(input):
     cards = process_input(input)
