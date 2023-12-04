3ce15816924ecec423ce711013326dd2058b110e
ddrmv <ddramchev@gmail.com>
2023-12-04 11:00:17 +0000

    refactor: Remove card number from card data

--- 2023/python/day04/y23_d04_p12.py
+++ 2023/python/day04/y23_d04_p12.py
@@ -1,31 +1,29 @@
 def process_input(input):
     cards = []
     for card in input.rstrip().split("\n"):
-        card_num_part, numbers_part = card.split(': ')
-        card_number = int(card_num_part.split(' ')[-1])
-        your_numbers_part, winning_numbers_part = numbers_part.split(' | ')
+        your_numbers_part, winning_numbers_part = card.split(': ')[1].split(' | ')
         your_numbers = [int(num) for num in your_numbers_part.split(' ') if num.isdigit()]
         winning_numbers = [int(num) for num in winning_numbers_part.split(' ') if num.isdigit()]
         card_copies = 1
-        cards.append([card_number, your_numbers, winning_numbers, card_copies])
+        cards.append([your_numbers, winning_numbers, card_copies])
     return cards
 
 def find_number_of_hits(card):
-    return len(set(card[1]).intersection(set(card[2])))
+    return len(set(card[0]).intersection(set(card[1])))
 
 def hits_to_value(hits):
     return hits if hits < 2 else 2**(hits - 1)
 
 def update_card_numbers(cards):
-    for card in cards:
+    for index, card in enumerate(cards):
         hits = find_number_of_hits(card)
         if hits != 0:
-            for next_card_num in range(int(card[0]), int(card[0]) + hits):
+            for next_card_num in range(index + 1, index + 1 + hits):
                 if next_card_num < len(cards):
-                    cards[next_card_num][3] += card[3]
+                    cards[next_card_num][2] += card[2]
     
 def get_total_number_of_cards(cards):
-    return sum([card[3] for card in cards])
+    return sum([card[2] for card in cards])
 
 def part1(input):
     cards = process_input(input)
