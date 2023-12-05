50de26783c316f59a5c5433a10fb64d193ff115d
ddrmv <ddramchev@gmail.com>
2023-12-05 11:00:35 +0000

    feat: Solution for 2023 day5 part1 in Python

---
+++ 2023/python/day05/y23_d05_p1.py
@@ -0,0 +1,64 @@
+from typing import List
+from functools import reduce
+
+class MapRange():
+    def __init__(self, dest_start, source_start, range_length):
+        self.dest_start = dest_start
+        self.source_start = source_start
+        self.range_length = range_length
+
+    def __str__(self):
+        return f"{self.dest_start},{self.source_start},{self.range_length};"
+
+    def is_in_range(self, num):
+        return self.source_start <= num < self.source_start + self.range_length
+    
+    def output_for(self, num):
+        '''Gives mapped destination for provided source'''
+        assert self.is_in_range(num) # to remove
+        return self.dest_start + (num - self.source_start)
+
+
+class GardenMap():
+    def __init__(self, name, map_ranges):
+        self.name = name
+        self.map_ranges = [MapRange(*mr) for mr in map_ranges]
+
+    def output_for(self, num):
+        '''Gives mapped destination for provided source'''
+        for mr in self.map_ranges:
+            if mr.is_in_range(num):
+                return mr.output_for(num)
+        return num
+    
+    def __str__(self):
+        output = f"m:{self.name} "
+        for mr in self.map_ranges:
+            output += str(mr)
+        return output
+
+def nums_str_to_nums_list(st):
+    return [int(num) for num in st.split(' ')]
+
+def parse_input(input):
+    seeds_part = input[:input.find('\n\n')]
+    seeds = nums_str_to_nums_list(seeds_part.split(': ')[1])
+    map_strings = input.rstrip().split('\n\n')[1:]
+    maps = []
+    for map_string in map_strings:
+        map_name = map_string[:map_string.find(' map')]
+        map_ranges_strings = map_string.split('\n')[1:]
+        map_ranges = [tuple(nums_str_to_nums_list(mrs)) for mrs in map_ranges_strings]
+        maps.append(GardenMap(map_name, map_ranges))
+    return seeds, maps
+
+def seed_to_location(seed: int, maps: List[GardenMap]):
+    next_step = seed
+    for m in maps:
+        next_step = m.output_for(next_step)
+    return next_step
+
+def part1(input):
+    seeds, maps = parse_input(input)
+    locations = [seed_to_location(seed, maps) for seed in seeds]
+    return min(locations)
---
+++ 2023/python/day05/y23_d05_p1_test.py
@@ -0,0 +1,71 @@
+import os, pytest
+from y23_d05_p1 import MapRange, GardenMap, seed_to_location, part1
+
+@pytest.fixture
+def example_input():
+    return '''seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4
+'''
+
+@pytest.fixture
+def full_input():
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, '../../input/day05.txt')
+    with open(filename) as file:
+        full_input = file.read()
+        return full_input
+
+
+@pytest.fixture
+def test_mr():
+    return MapRange(50, 98, 2)
+
+@pytest.fixture
+def test_gm():
+    return GardenMap("seed-to-soil", [(50, 98, 2), (52, 50, 48)])
+
+
+def test_MapRange(test_mr):
+    assert test_mr.is_in_range(97) == False
+    assert test_mr.is_in_range(99) == True
+    assert test_mr.output_for(99) == 51
+
+def test_GardenMap(test_gm):
+    assert test_gm.output_for(99) == 51
+    assert test_gm.output_for(200) == 200
+
+
+def test_part1(example_input, full_input):
+    assert part1(example_input) == 35
+    assert part1(full_input) == 199602917
