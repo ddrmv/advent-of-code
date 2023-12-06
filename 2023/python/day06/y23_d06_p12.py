b302749db0c6b9630247278d25f626f55936a10b
ddrmv <ddramchev@gmail.com>
2023-12-06 11:28:11 +0000

    perf: Use bisect to find min and max speed wins for y23 d6 p2

--- 2023/python/day06/y23_d06_p12.py
+++ 2023/python/day06/y23_d06_p12.py
@@ -1,5 +1,6 @@
 from functools import reduce
 from typing import List
+from bisect import bisect_left
 
 
 class Race():
@@ -23,6 +24,20 @@ class Race():
             if self.is_win(speed):
                 ways += 1
         return ways
+    
+    def find_a_win_logn(self, min, max):
+        if self.is_win((max - min) // 2):
+            return (max - min) // 2
+        return self.find_a_win_logn(min, max//2) or self.find_a_win_logn(max//2, max)
+
+    def ways_to_win_bisect(self):
+        found_win = self.find_a_win_logn(1, self.time)
+        speeds_upto_found_win = list(range(1, found_win + 1))
+        min_win_speed = bisect_left(speeds_upto_found_win, 1, key=lambda speed: self.is_win(speed))
+        speeds_after_found_win = list(range(self.time, found_win, -1))
+        position = bisect_left(speeds_after_found_win, 1, key=lambda speed: self.is_win(speed))
+        max_win_speed = speeds_after_found_win[position]
+        return max_win_speed - min_win_speed
 
 
 def nums_str_to_nums_list(st):
@@ -48,11 +63,12 @@ def process_input_p2(input):
 
 
 def part1(input):
-    races = List[Race]
+    races: List[Race]
     races = process_input(input)
     ways_list = [race.ways_to_win() for race in races]
     return reduce(lambda a,b: a*b, ways_list)
 
 def part2(input):
+    race: Race
     race = process_input_p2(input)
-    return race.ways_to_win()
+    return race.ways_to_win_bisect()
