75d2638945acfecbb6acbde02ec25ff12a429637
ddrmv <ddramchev@gmail.com>
2023-12-11 22:48:20 +0000

    refactor: Change grid to Universe class

--- 2023/python/day11/y23_d11_p1.py
+++ 2023/python/day11/y23_d11_p1.py
@@ -1,5 +1,36 @@
 from typing import List
 
+class Universe():
+    def __init__(self, input_array):
+        self.grid: List[List[str]]
+        self.grid = input_array
+
+    def is_col_empty(self, col):
+        for row in self.grid:
+            if row[col] != '.':
+                return False
+        return True
+
+    def expand_universe(self):
+        row_len = len(self.grid[0])
+        empty_rows = [index for index, row in enumerate(self.grid) if row.count('.') == row_len]
+        empty_cols = [col for col in range(0, row_len) if self.is_col_empty(col)]
+        empty_row_fill = self.grid[empty_rows[0]]
+        for empty_row_index in reversed(empty_rows):
+            self.grid.insert(empty_row_index, empty_row_fill.copy())
+        for row in self.grid:
+            for empty_col_index in reversed(empty_cols):
+                row.insert(empty_col_index, '.')
+
+    def __str__(self):
+        output = ''
+        for row in self.grid:
+            for char in row:
+                output += char
+            output += '\n'
+        return output
+
+
 def process_input(input):
     grid = []
     for line in input.rstrip().split('\n'):
@@ -7,36 +38,9 @@ def process_input(input):
         grid.append(new_array)
     return grid
 
-def is_col_empty(grid, col):
-    for row in grid:
-        if row[col] != '.':
-            return False
-    return True
-
-def expand_universe(grid: List[List[str]]):
-    row_len = len(grid[0])
-    empty_rows = [index for index, row in enumerate(grid) if row.count('.') == row_len]
-    empty_cols = [col for col in range(0, row_len) if is_col_empty(grid, col)]
-    empty_row_fill = grid[empty_rows[0]]
-    for empty_row_index in reversed(empty_rows):
-        grid.insert(empty_row_index, empty_row_fill.copy())
-    for row in grid:
-        for empty_col_index in reversed(empty_cols):
-            row.insert(empty_col_index, '.')
-    return grid
-
-def print_universe(grid):
-    output = ''
-    for row in grid:
-        for char in row:
-            output += char
-        output += '\n'
-    return output
-
 
 def part1(input):
-    grid = process_input(input)
-    # print(grid[0])
-    expand_universe(grid)
-    print(print_universe(grid))
-    return 0
+    u = Universe(process_input(input))
+    u.expand_universe()
+    print(u)
+    return u
